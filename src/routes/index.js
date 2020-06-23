162dc8109e928199ae07a89c0956f4f8ed2bd6aa
Carlos Echeverria <echeverriadev@gmail.com>
2020-06-23 16:00:11 -0400

    run good

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,21 +11,41 @@ cloudinary.config({
 });
 
 router.get('/', async (req, res) => {
-  //esta variable photos es de tipo object
-  // const photos = await Photo.find();
+  const photos = await Photo.find();
 
-  const photos = [
-    { title: 'joda', text: 'text a' },
-    { title: 'creo', text: 'text b' },
-    { title: 'lo consegui', text: 'text c' },
-  ];
-  console.log(typeof photos);
-  res.render('images', { photos });
+  const newPhotos = photos.map(
+    ({ title, description, imageURL, public_id }) => {
+      const objetico = {
+        title,
+        description,
+        imageURL,
+        public_id,
+      };
+
+      return objetico;
+    }
+  );
+  res.render('images', { newPhotos });
 });
 
 router.get('/image/add', async (req, res) => {
   const photos = await Photo.find();
-  res.render('image_form', { photos });
+
+  const newPhotos = photos.map(
+    ({ _id, title, description, imageURL, public_id }) => {
+      const objetico = {
+        _id,
+        title,
+        description,
+        imageURL,
+        public_id,
+      };
+
+      return objetico;
+    }
+  );
+
+  res.render('image_form', { newPhotos });
 });
 
 router.post('/image/add', async (req, res) => {
@@ -39,10 +59,19 @@ router.post('/image/add', async (req, res) => {
     imageURL: result.secure_url,
     public_id: result.public_id,
   });
-  console.log(newPhoto.imageURL);
+
   await newPhoto.save();
   await fs.unlink(req.file.path);
-  res.send('recibido');
+  res.redirect('/');
+});
+
+router.get('/image/delete/:photo_id', async (req, res) => {
+  const { photo_id } = req.params;
+  const photo = await Photo.findByIdAndDelete(photo_id);
+  const result = await cloudinary.v2.uploader.destroy(photo.public_id);
+
+  console.log(result);
+  res.redirect('/image/add');
 });
 
 module.exports = router;
